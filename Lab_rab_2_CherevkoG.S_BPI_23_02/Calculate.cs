779de0f7b42fc6ae136c8646f52dd769e8e5cb91
zzakazzaklol@gmail.com <zzakazzaklol@gmail.com>
2025-09-29 16:09:29 +0500

    Создан класс для расчета функции и выбора элементов в комбо-боксе

---
+++ Lab_rab_2_CherevkoG.S_BPI_23_02/Calculate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_rab_2_CherevkoG.S_BPI_23_02
+{
+    public static class Calculate
+    {
+        public static double CalcZ(int N, int K, double p, double x, double f, double y)
+        {
+            double sum = 0.0;
+            double ysq = y * y;
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= K; j++)
+                {
+                    double chisl = System.Math.Pow(p, i - 1) * x + System.Math.Pow(f, j - 1) * ysq;
+                    double znam = i * j;
+                    sum += chisl / znam;
+                }
+            }
+            return sum;
+        }
+
+        public static List<int> GetNValues()
+        {
+            return new List<int> { 1, 2, 3, 4, 5, 10, 15, 20 };
+        }
+
+        public static List<int> GetKValues()
+        {
+            return new List<int> { 1, 2, 3, 4, 5, 10, 15, 20 };
+        }
+
+        public static List<double> GetOtherValues()
+        {
+            return new List<double> { 0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 5.0, 10.0 };
+        }
+    }
+}
